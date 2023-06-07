bf20de187d220bfb24316496ec91c80e7ab43940
ryanzhao <ryanzhaors@qq.com>
2023-06-07 17:38:17 +1000

    WIP: unit test of message receiver for disappearing messages

---
+++ SessionMessagingKitTests/Sending & Receiving/MessageReceiverDisappearingMessagesSpec.swift
@@ -0,0 +1,22 @@
+// Copyright © 2023 Rangeproof Pty Ltd. All rights reserved.
+
+import Foundation
+import Sodium
+import GRDB
+import SessionUtilitiesKit
+
+import Quick
+import Nimble
+
+@testable import SessionMessagingKit
+
+class MessageReceiverDisappearingMessagesSpec: QuickSpec {
+    // MARK: - Spec
+
+    override func spec() {
+        var mockStorage: Storage!
+        var dependencies: SMKDependencies!
+        
+        
+    }
+}
