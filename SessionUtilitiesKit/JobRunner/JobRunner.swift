f9dc85b7ab8030b4503afe1682d02786a9a4bd3b
Morgan Pretty <morgan.t.pretty@gmail.com>
2023-10-02 12:59:52 +1100

    Fixed a couple of onboarding bugs and tweaks some logs
    Fixed an issue where starting and then restarting onboarding would get the app into an invalid state
    Fixed an issue where newline characters could cause issues with parsing the recovery phrase
    Added additional logs to better be able to understand the database state

--- SessionUtilitiesKit/JobRunner/JobRunner.swift
+++ SessionUtilitiesKit/JobRunner/JobRunner.swift
@@ -1096,6 +1096,7 @@ public final class JobQueue: Hashable {
         hasStartedAtLeastOnce.mutate { $0 = true }
         
         // Get any pending jobs
+        let jobVariants: [Job.Variant] = self.jobVariants
         let jobIdsAlreadyRunning: Set<Int64> = currentlyRunningJobIds.wrappedValue
         let jobsAlreadyInQueue: Set<Int64> = pendingJobsQueue.wrappedValue.compactMap { $0.id }.asSet()
         let jobsToRun: [Job] = dependencies.storage.read(using: dependencies) { db in
@@ -1320,6 +1321,7 @@ public final class JobQueue: Hashable {
     }
     
     private func scheduleNextSoonestJob(using dependencies: Dependencies) {
+        let jobVariants: [Job.Variant] = self.jobVariants
         let jobIdsAlreadyRunning: Set<Int64> = currentlyRunningJobIds.wrappedValue
         let nextJobTimestamp: TimeInterval? = dependencies.storage.read(using: dependencies) { db in
             try Job
