18852b8a522023fad0603593eecfe631adcb70e5
Ryan Zhao <ryanzhaors@qq.com>
2023-10-30 15:38:36 +1100

    fix an issue where closed group disappearing messages type can be set to unknown by shared config

--- SessionMessagingKit/SessionUtil/Config Handling/SessionUtil+UserGroups.swift
+++ SessionMessagingKit/SessionUtil/Config Handling/SessionUtil+UserGroups.swift
@@ -89,6 +89,7 @@ internal extension SessionUtil {
                             .with(
                                 isEnabled: (legacyGroup.disappearing_timer > 0),
                                 durationSeconds: TimeInterval(legacyGroup.disappearing_timer),
+                                type: .disappearAfterSend,
                                 lastChangeTimestampMs: latestConfigSentTimestampMs
                             ),
                         groupMembers: members
