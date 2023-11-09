6ac030765006f63870956812a3badea4e7697cf4
Ryan ZHAO <>
2023-11-09 13:18:10 +1100

    clean

--- SessionUIKit/Utilities/SwiftUI+Utilities.swift
+++ SessionUIKit/Utilities/SwiftUI+Utilities.swift
@@ -23,21 +23,6 @@ extension EnvironmentValues {
     }
 }
 
-extension UIViewController {
-    public func present<Content: View>(style: UIModalPresentationStyle = .automatic, @ViewBuilder builder: () -> Content) {
-        let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
-        toPresent.modalPresentationStyle = style
-        toPresent.rootView = AnyView(
-            builder()
-                .environment(\.viewController, toPresent)
-        )
-        NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "dismissModal"), object: nil, queue: nil) { [weak toPresent] _ in
-            toPresent?.dismiss(animated: true, completion: nil)
-        }
-        self.present(toPresent, animated: true, completion: nil)
-    }
-}
-
 public struct UIView_SwiftUI: UIViewRepresentable {
     public typealias UIViewType = UIView
     
