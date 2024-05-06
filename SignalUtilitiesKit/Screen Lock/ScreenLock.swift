eb8f2d3c2696bfbfed167ab198244561c4dac724
Ryan ZHAO <>
2024-05-06 13:12:10 +1000

    minor fix

--- SignalUtilitiesKit/Screen Lock/ScreenLock.swift
+++ SignalUtilitiesKit/Screen Lock/ScreenLock.swift
@@ -53,7 +53,7 @@ public class ScreenLock {
             // Description of how and why Signal iOS uses Touch ID/Face ID/Phone Passcode to
             // unlock 'screen lock'.
             localizedReason: "authenticateToOpen"
-                .put(key: "app_name", value: Singleton.appContext.appName)
+                .put(key: "app_name", value: Singleton.appName)
                 .localized()
         ) { outcome in
             AssertIsOnMainThread()
