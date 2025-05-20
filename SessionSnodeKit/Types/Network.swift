fa18411258875851fbcce0accd5bddad5e8a50a5
Ryan ZHAO <>
2025-05-20 16:03:41 +1000

    clean up TODOs

--- SessionSnodeKit/Types/Network.swift
+++ SessionSnodeKit/Types/Network.swift
@@ -56,7 +56,6 @@ public enum NetworkStatus {
 
 public extension Network {
     enum NetworkAPI {
-        // TODO: Update to production env
         static let networkAPIServer = "http://networkv1.getsession.org"
         static let networkAPIServerPublicKey = "cbf461a4431dc9174dceef4421680d743a2a0e1a3131fc794240bcb0bc3dd449"
         
--- SessionUtilitiesKit/Database/Models/Setting.swift
+++ SessionUtilitiesKit/Database/Models/Setting.swift
@@ -4,6 +4,7 @@ import Foundation
 import GRDB
 
 // MARK: - Setting
+// TODO: Refactor the usage of these in future release
 
 public typealias Setting = KeyValueStore
 public typealias EnumIntSetting = EnumInt
