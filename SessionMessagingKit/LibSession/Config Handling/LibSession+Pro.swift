fe80abffc51db1cabe00f9026097d57534ddc6e7
Ryan ZHAO <>
2025-06-23 16:31:53 +1000

    add Session Pro Badge

---
+++ SessionMessagingKit/LibSession/Config Handling/LibSession+Pro.swift
@@ -0,0 +1,23 @@
+// Copyright © 2025 Rangeproof Pty Ltd. All rights reserved.
+
+import Foundation
+import GRDB
+import SessionUtil
+import SessionUtilitiesKit
+
+// MARK: - Character Limits
+
+public extension LibSession {
+    static var CharacterLimit: Int { 2000 }
+    static var ProCharacterLimit: Int { 10000 }
+    
+    static func numberOfCharactersLeft(for content: String, isSessionPro: Bool) -> Int {
+        return ((isSessionPro ? ProCharacterLimit : CharacterLimit) - content.count)
+    }
+}
+
+// MARK: - Session Pro
+
+public extension LibSessionCacheType {
+    var isSessionPro: Bool { return false }
+}
