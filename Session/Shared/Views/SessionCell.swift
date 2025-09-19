5c12f47a207604f90e0b01c84d473e104ad16f4e
Morgan Pretty <morgan.t.pretty@gmail.com>
2025-09-19 14:46:42 +1000

    Fixed some merge issues

--- Session/Shared/Views/SessionCell.swift
+++ Session/Shared/Views/SessionCell.swift
@@ -530,6 +530,7 @@ public class SessionCell: UITableViewCell {
         titleLabel.accessibilityIdentifier = info.title?.accessibility?.identifier
         titleLabel.accessibilityLabel = info.title?.accessibility?.label
         titleLabel.isHidden = (info.title == nil)
+        titleLabel.attachTrailing(info.title?.textTailing)
         subtitleLabel.isUserInteractionEnabled = (info.subtitle?.interaction == .copy)
         subtitleLabel.font = info.subtitle?.font
         subtitleLabel.themeTextColor = info.styling.subtitleTintColor
