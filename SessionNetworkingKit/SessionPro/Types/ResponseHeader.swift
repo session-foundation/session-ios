3de30f866521dfd3c438cd5f9adf69579b475539
Ryan ZHAO <>
2026-02-18 16:10:58 +1100

    fix: do not handle errors in already redeem status

--- SessionNetworkingKit/SessionPro/Types/ResponseHeader.swift
+++ SessionNetworkingKit/SessionPro/Types/ResponseHeader.swift
@@ -7,6 +7,9 @@ public extension Network.SessionPro {
     struct ResponseHeader: Equatable {
         public let status: UInt32
         public let errors: [String]
+        public var isSuccess: Bool {
+            errors.isEmpty || status == AddProPaymentResponseStatus.alreadyRedeemed.libSessionValue.rawValue
+        }
         
         init(_ libSessionValue: session_pro_backend_response_header) {
             status = libSessionValue.status
