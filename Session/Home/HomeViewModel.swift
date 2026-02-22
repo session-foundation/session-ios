7f0b3ee499193abb7b3941d7d7fc3ac4e359745f
Morgan Pretty <morgan.t.pretty@gmail.com>
2026-02-23 10:42:05 +1100

    Fix bug where home screen settings button wouldn't update correctly

--- Session/Home/HomeViewModel.swift
+++ Session/Home/HomeViewModel.swift
@@ -95,7 +95,7 @@ public class HomeViewModel: NavigatableStateHolder {
         }
         
         let viewState: ViewState
-        let userProfile: Profile
+        let userSessionId: SessionId
         let serviceNetwork: ServiceNetwork
         let forceOffline: Bool
         let hasSavedThread: Bool
@@ -113,6 +113,11 @@ public class HomeViewModel: NavigatableStateHolder {
         let appWasInstalledPriorToAppReviewRelease: Bool
         let showVersionSupportBanner: Bool
         let showDonationsCTAModal: Bool
+        
+        var userProfile: Profile {
+            dataCache.profile(for: userSessionId.hexString) ??
+            Profile.defaultFor(userSessionId.hexString)
+        }
         
         @MainActor public func sections(viewModel: HomeViewModel) -> [SectionModel] {
             HomeViewModel.sections(state: self, viewModel: viewModel)
@@ -161,7 +166,7 @@ public class HomeViewModel: NavigatableStateHolder {
             
             return State(
                 viewState: .loading,
-                userProfile: Profile.with(id: userSessionId.hexString, name: ""),
+                userSessionId: userSessionId,
                 serviceNetwork: dependencies[feature: .serviceNetwork],
                 forceOffline: dependencies[feature: .forceOffline],
                 hasSavedThread: false,
@@ -205,7 +210,7 @@ public class HomeViewModel: NavigatableStateHolder {
         using dependencies: Dependencies
     ) async -> State {
         let startedAsNewUser: Bool = (await dependencies[singleton: .onboarding].initialFlow == .register)
-        var userProfile: Profile = previousState.userProfile
+        let userSessionId: SessionId = previousState.userSessionId
         var serviceNetwork: ServiceNetwork = previousState.serviceNetwork
         var forceOffline: Bool = previousState.forceOffline
         var hasSavedThread: Bool = previousState.hasSavedThread
@@ -228,6 +233,8 @@ public class HomeViewModel: NavigatableStateHolder {
         
         /// If this is the initial query then we need to properly fetch the initial state
         if isInitialQuery {
+            var userProfile: Profile = Profile.defaultFor(userSessionId.hexString)
+            
             /// Insert a fake event to force the initial page load
             eventsToProcess.append(ObservedEvent(
                 key: .loadPage(HomeViewModel.self),
@@ -452,7 +459,7 @@ public class HomeViewModel: NavigatableStateHolder {
                 .empty(isNewUser: (startedAsNewUser && !hasSavedThread && !hasSavedMessage)) :
                 .loaded
             ),
-            userProfile: userProfile,
+            userSessionId: userSessionId,
             serviceNetwork: serviceNetwork,
             forceOffline: forceOffline,
             hasSavedThread: hasSavedThread,
